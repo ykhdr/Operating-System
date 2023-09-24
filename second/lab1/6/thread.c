5dd34a15846391ff15348c3ae69db446e641f5f9
ykhdr <yanhudorozhkov@gmail.com>
2023-09-24 14:04:42 +0700

    Added 6 lab

--- second/lab1/6/thread.c
+++ second/lab1/6/thread.c
@@ -1,73 +1,155 @@
 #define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
 #include <sched.h>
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
-#include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define STACK_SIZE (1024 * 1024)
 
-typedef struct {
-    void *(*start_routine)(void *); 
-    void *arg;
-} thread_data_t;
+#define PAGE 4096
+#define STACK_SIZE PAGE*8
 
-static int thread_function(void *arg) {
-    thread_data_t *data = (thread_data_t *)arg;
-    data->start_routine(data->arg);
+typedef void *(*start_routine_t)(void *);
+
+typedef struct _mythread {
+    int                 mythread_id;
+    start_routine_t     start_routine;
+    void*               arg;
+    void*               retval;
+    volatile int        joined;          
+    volatile int        finished;   
+} mythread_struct_t;
+
+typedef mythread_struct_t* mythread_t;
+
+void* create_stack(size_t size, int tid){
+    char stack_file[128];
+    int stack_fd;
+    void *stack;
+
+    snprintf(stack_file, sizeof(stack_file),"stack-%d",tid);
+
+    stack_fd = open(stack_file, O_RDWR | O_CREAT, 0660);
+    ftruncate(stack_fd,0);
+    ftruncate(stack_fd,size);
+
+    stack = mmap(NULL,size,PROT_READ|PROT_WRITE, MAP_SHARED, stack_fd, 0);
+    close(stack_fd);
+
+    return stack;
+}
+
+
+int mythread_startup(void *arg){
+    mythread_t tid = (mythread_t) arg;
+    mythread_struct_t *mythread = tid;
+
+    printf("thread_startup: start thread function for thread %d\n",mythread->mythread_id);
+
+    mythread->retval = mythread->start_routine(mythread->arg);
+
+    mythread->finished = 1;
+
+    printf("thread_startup: waiting for join() the thread %d\n",mythread->mythread_id);
+    while(mythread->joined){
+        usleep(1);
+    }
+
+    printf("thread_startup: the thread function finished for the thread %d\n",mythread->mythread_id);
+
     return 0;
 }
 
-int mythread_create(pthread_t *thread, void *(*start_routine)(void *), void *arg) {
-    thread_data_t data;
-    data.start_routine = start_routine;
-    data.arg = arg;
+int mythread_create(mythread_t *mytid, start_routine_t start_routine, void* arg){
+    static int tid = 0;
+    mythread_struct_t *mythread;
+    void* child_stack;
+    int child_pid;
 
-    void *stack = malloc(STACK_SIZE);
-    if (stack == NULL) {
-        perror("Error allocating stack");
-        return -1;
-    }
-
-    *thread = clone(thread_function,
-		   	stack + STACK_SIZE,
-		   	CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID,
-		   	&data,
-		   	NULL, NULL, NULL);
+    tid++;
     
-    if (*thread == -1) {
-        perror("Error creating thread");
-        free(stack);
+    printf("mythread_create: creating thread %d\n", tid);
+
+    child_stack = create_stack(STACK_SIZE, tid);
+    mprotect(child_stack+PAGE, STACK_SIZE - PAGE, PROT_READ|PROT_WRITE);
+
+    memset(child_stack+PAGE,0x7f,STACK_SIZE-PAGE);
+    
+    mythread = (mythread_struct_t*)(child_stack + STACK_SIZE - sizeof(mythread_struct_t));
+    mythread->mythread_id = tid;
+    mythread->start_routine = start_routine;
+    mythread->arg = arg; 
+    mythread->retval = NULL;
+    mythread->finished = 0;
+    mythread->joined = 0;
+
+
+    child_stack = (void*)mythread;
+
+    printf("child stack %p; mythread_struct %p; \n", child_stack, mythread);
+
+    child_pid =  clone(mythread_startup,child_stack, CLONE_VM | CLONE_FILES | CLONE_THREAD | CLONE_SIGHAND | SIGCHLD, (void*)mythread);
+    if(child_pid == -1){
+        fprintf(stderr,"clone failed: %s\n",strerror(errno));
         return -1;
     }
 
+    *mytid = mythread;
+
     return 0;
 }
 
-void * thread_runnable(void* arg){
-	printf(arg);
-}
+void mythread_join(mythread_t mytid, void** retval){
+    mythread_struct_t *mythread = mytid;
 
-int main() {
-    pthread_t my_thread;
-    int value = 42;
-	int err;
+    printf("mythread_join: waiting for the therad %d to finish\n", mythread->mythread_id);
 
-
-    err = mythread_create(&my_thread, thread_runnable, (void *)"Hello world!!\n");
-      
-	if(err == 0){  
-		printf("mythread_create succeeded\n");
-    } else {
-        printf("mythread_create failed\n");
-        return 1;
+    while(!mythread->finished){
+        usleep(1);
     }
 
-    waitpid(my_thread, NULL, 0);
+    printf("mythread_join: the thread %d finished\n", mythread->mythread_id);
 
-    return 0;
+    *retval = mythread->retval;
+    
+    mythread->joined = 1;
+}
+
+void* mythread(void *arg){
+    char* str = (char*) arg;
+    for(int i = 0; i < 5; i++){
+        printf("hello: '%s'\n", str);
+        sleep(1);
+    }
+
+    return "retval!";
+}
+
+int main(){
+    mythread_t tid1;
+    mythread_t tid2;
+    
+ 
+    void* retval1;
+    void* retval2;
+    
+    mythread_create(&tid1,mythread, "hello from thread1");
+    sleep(2);
+    mythread_create(&tid2,mythread, "hello from thread2");
+    
+    mythread_join(tid1,&retval1);
+
+    printf("main: thread1 returned value : %s\n", (char*) retval1);
+
+    mythread_join(tid2,&retval2);
+
+    printf("main: thread2 returned value : %s\n", (char*) retval2);
 }
