aee80f4a3d0332d5044cb74de6b343f18607a813
ykhdr <yanhudorozhkov@gmail.com>
2023-12-02 14:19:04 +0700

    Added 2.3 lab

---
+++ second/lab2/3/list.c
@@ -0,0 +1,51 @@
+#include "list.h"
+
+
+storage_t *initialize_storage(const int capacity) {
+    storage_t *storage = malloc(sizeof(storage_t));
+    if (!storage) {
+        printf("Failed to allocate memory for a queue\n");
+        abort();
+    }
+    storage->capacity = capacity;
+    storage->first = NULL;
+    return storage;
+}
+
+void add_node(storage_t *storage, const char *value) {
+    node_t *new_node = malloc(sizeof(node_t));
+
+    if (!new_node) {
+        perror("Failed to allocate memory for a new node");
+        exit(EXIT_FAILURE);
+    }
+    if (storage->first != NULL) {
+        node_t *node = storage->first;
+        while (node->next != NULL) {
+            node = node->next;
+        }
+        node->next = new_node;
+    } else {
+        storage->first = new_node;
+    }
+    strcpy(new_node->value, value);
+    new_node->next = NULL;
+    pthread_mutex_init(&(new_node->sync), NULL);
+}
+
+void fill_storage(storage_t *storage) {
+    for (int i = 0; i < storage->capacity; ++i) {
+        char buff[10];
+        sprintf(buff, "%d", (i) % storage->capacity);
+        add_node(storage, buff);
+    }
+}
+
+void print_storage(const storage_t *storage) {
+    node_t *current = storage->first;
+    while (current != NULL) {
+        printf("%s ", current->value);
+        current = current->next;
+    }
+    printf("\n");
+}
